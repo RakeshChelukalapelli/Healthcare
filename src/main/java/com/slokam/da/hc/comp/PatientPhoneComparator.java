474ec379599fde14af40f003985f48a6a34a5efb
Rakesh <rakesh.chelukalapelli@gmail.com>
2024-01-10 22:57:29 +0530

    changes

--- src/main/java/com/slokam/da/hc/comp/PatientPhoneComparator.java
+++ src/main/java/com/slokam/da/hc/comp/PatientPhoneComparator.java
@@ -8,7 +8,11 @@ public class PatientPhoneComparator implements Comparator<Patient> {
 @Override
 public int compare(Patient o1, Patient o2) {
 //compare
+<<<<<<< Updated upstream
 //comapre
+=======
+//save
+>>>>>>> Stashed changes
 	return o1.getPhone().compareTo(o2.getPhone());
 }
 }
